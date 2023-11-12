5c006b5864b280ae02a0395212414296fff92bc4
LuisSpielmann <127334574+Aomyine@users.noreply.github.com>
2023-11-12 22:57:22 +0000

    Pending changes exported from your codespace

--- prog2/src/main/java/com/restapi/prog2/Prog2Application.java
+++ prog2/src/main/java/com/restapi/prog2/Prog2Application.java
@@ -1,13 +1,15 @@
 package com.restapi.prog2;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.restapi.prog2.repositorios")
+@EntityScan(basePackages = "com.restapi.prog2.classes")
 public class Prog2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Prog2Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Prog2Application.class, args);
+    }
 }
