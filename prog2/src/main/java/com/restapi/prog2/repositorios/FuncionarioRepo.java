fd3a3305f9d18e620c5447a534be820f9a57c2ce
LuisSpielmann <127334574+Aomyine@users.noreply.github.com>
2023-11-12 15:17:46 -0300

    Delete prog2 directory
    Signed-off-by: LuisSpielmann <127334574+Aomyine@users.noreply.github.com>

--- prog2/src/main/java/com/restapi/prog2/repositorios/FuncionarioRepo.java
+++
@@ -1,9 +0,0 @@
-package com.restapi.prog2.repositorios;
-import com.restapi.prog2.classes.Funcionario;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FuncionarioRepo extends CrudRepository<Funcionario, Integer> {
-    
-}
--- prog2/src/main/java/com/restapi/prog2/repositorios/ProdutoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.restapi.prog2.repositorios;
-import com.restapi.prog2.classes.Produto;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepo extends CrudRepository<Produto, Integer> {
-    
-}
