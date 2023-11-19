5142aa35cb7c7fad85c314ba732204d5301333b7
LuisSpielmann <127334574+Aomyine@users.noreply.github.com>
2023-11-19 17:13:08 -0300

    Update ContaBancariaController.java
    Signed-off-by: LuisSpielmann <127334574+Aomyine@users.noreply.github.com>

--- prog2/src/main/java/com/restapi/prog2/controllers/ContaBancariaController.java
+++ prog2/src/main/java/com/restapi/prog2/controllers/ContaBancariaController.java
@@ -1,5 +1,6 @@
 package com.restapi.prog2.controllers;
 import java.util.*;
+
 import com.restapi.prog2.classes.ContaBancaria;
 import com.restapi.prog2.repositorios.ContaBancariaRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,35 +16,35 @@ public class ContaBancariaController {
     private ContaBancariaRepo contaBancariaRepo;
 
     @GetMapping("/api/Contas")
-    public Iterable<ContaBancaria> getContas() {
+    public Iterable<ContaBancaria> getContaBancaria() {
         return contaBancariaRepo.findAll();
     }
 
     @GetMapping("/api/Contas/{id}")
-    public Optional<ContaBancaria> getConta(@PathVariable int id) {
+    public Optional<ContaBancaria> getContaBancaria (@PathVariable long id) {
         return contaBancariaRepo.findById(id);
     }
 
-    @PostMapping("/api/Contas")
-    public ContaBancaria createConta(@RequestBody ContaBancaria contaBancaria) {
-        ContaBancaria createdConta = contaBancariaRepo.save(contaBancaria);
-        return createdConta;
-    }
+	@PostMapping("/api/Contas")
+	ContaBancaria createContaBancaria(@RequestBody ContaBancaria c) {
+		ContaBancaria createdCon = contaBancariaRepo.save(c);
+		return createdCon;
+	}
 
-    @PutMapping("/api/Contas/{TitularId}")
-    public int updateContaBancaria(@RequestBody ContaBancaria contaRequest, @PathVariable int TitularId) {
-        Optional<ContaBancaria> opt = contaBancariaRepo.findById(TitularId);
-        if (opt.isPresent()) {
-            if (contaRequest.getIdTitular() == TitularId) {
-                contaBancariaRepo.save(contaRequest);
-                return TitularId;
-            }
-        }
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Erro ao alterar dados da conta com id " + TitularId);
-    }
+    @PutMapping("/api/Contas/{idTitular}")
+	Optional<ContaBancaria> updateContaBancaria(@RequestBody ContaBancaria contaReq, @PathVariable long idTitular) {
+		Optional<ContaBancaria> opt = contaBancariaRepo.findById(idTitular);
+		if (opt.isPresent()) {
+			if (contaReq.getIdTitular() == idTitular) {
+				contaBancariaRepo.save(contaReq);
+				return opt;
+			}
+		}
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Erro ao alterar dados da conta com id " + idTitular);
+	}	
 
-    @DeleteMapping("/api/Contas/{id}")
-    public void deleteConta(@PathVariable int id) {
-        contaBancariaRepo.deleteById(id);
+	@DeleteMapping("/api/Contas/{id}")
+	void deleteConta(@PathVariable long id) {
+		contaBancariaRepo.deleteById(id);
     }
 }
