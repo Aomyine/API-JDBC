1ff44cfe709b2cc173630c6715501c446e5bfa2b
LuisSpielmann <127334574+Aomyine@users.noreply.github.com>
2023-11-19 17:15:21 -0300

    Update CidadeRepo.java
    Signed-off-by: LuisSpielmann <127334574+Aomyine@users.noreply.github.com>

--- prog2/src/main/java/com/restapi/prog2/repositorios/CidadeRepo.java
+++ prog2/src/main/java/com/restapi/prog2/repositorios/CidadeRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CidadeRepo extends CrudRepository<Cidade, Integer> {
+public interface CidadeRepo extends CrudRepository<Cidade, Long> {
     
 }
