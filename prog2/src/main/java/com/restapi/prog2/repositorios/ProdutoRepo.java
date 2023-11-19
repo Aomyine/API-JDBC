b46c5b399eaa031a04ec1997577b9d76b22be8e0
LuisSpielmann <127334574+Aomyine@users.noreply.github.com>
2023-11-19 17:17:01 -0300

    Update ProdutoRepo.java
    Signed-off-by: LuisSpielmann <127334574+Aomyine@users.noreply.github.com>

--- prog2/src/main/java/com/restapi/prog2/repositorios/ProdutoRepo.java
+++ prog2/src/main/java/com/restapi/prog2/repositorios/ProdutoRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepo extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepo extends CrudRepository<Produto, Long> {
     
 }
